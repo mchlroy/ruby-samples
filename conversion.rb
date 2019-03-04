9d0a6a7ba9b4f2d2fb10433a79cf2a7d59fb704e
Michaël Roy <michael.roy3@usherbrooke.ca>
2019-03-04 14:07:40 -0500

    First commit

---
+++ conversion.rb
@@ -0,0 +1,8 @@
+a = "1"
+number = a.to_i
+
+b = "1.05"
+float = b.to_f
+
+puts number * 100
+puts float * 2
---
+++ input.rb
@@ -0,0 +1,25 @@
+# chomp is to remove trailing \n ex: str = "Hi\n"
+# str.chomp == "Hi"
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+# Should use stdin.gets.chomp if using ARGV (exec arguments)
